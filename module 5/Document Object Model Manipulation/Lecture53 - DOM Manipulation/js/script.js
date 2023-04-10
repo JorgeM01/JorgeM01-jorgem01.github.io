27596feb8923a47b36761989f425bb8944e811d6
JorgeML01 <jorgedd2001@gmail.com>
2023-04-09 20:36:17 -0600

    first js with html

---
+++ module 5/Document Object Model Manipulation/Lecture53 - DOM Manipulation/js/script.js
@@ -0,0 +1,34 @@
+// DOM manipulation.
+console.log(document.getElementById("title"));
+//HTMLDocument sería como decir instancia de window.
+console.log(document instanceof HTMLDocument);
+
+function sayHello() {
+  // Con el value podemos obtener el valor de ese input y no nos retorna
+  // la línea del html de ese id como lo hace sin el value.
+  var name = document.getElementById("name").value;
+  var message = "<h1>Hello " + name + "!</h1>";
+
+  //console.log(document.getElementById("name").value);
+
+  //! De esta forma solamente lo toma como texto normal.
+  //document.getElementById("content").textContent = message;
+
+  //! De esta otra forma lo toma como código de html. Podemos incluir tags.
+  document.getElementById("content").innerHTML = message;
+
+  //* Agregándole más cosas:
+  if (name.toLowerCase() === "Jordan Omar Matute Lopez".toLowerCase()) {
+    var title =
+      //! Esta es una mejor forma que el getElementById porque lo restringe a
+      //! solo poder seleccionar por id. Entonces esta otra forma es más genérica.
+      document.querySelector("#title").textContent; // Es como cuando se seleecionan varios divs a la vez en css y así.
+    title += " esto le agregué al texto.";
+
+    //! Así se actualiza porque sería como modificarlo por referencia al título.
+    // Ojo: con esta function toma el primer elemento, en este caso al primer h1.
+    // Existe otra función para sí tomar a todos. Esto sería el equivalente a usar #title
+    // que usé arriba. De hecho aquí también podría paonerlo.
+    document.querySelector("h1").textContent = title;
+  }
+}
