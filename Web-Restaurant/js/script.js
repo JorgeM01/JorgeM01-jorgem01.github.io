5cbab37ece8e533c3526576815678faaea9d4f30
JorgeML01 <jorgedd2001@gmail.com>
2023-04-12 02:31:47 -0600

    blur update

--- Web-Restaurant/js/script.js
+++ Web-Restaurant/js/script.js
@@ -4,6 +4,7 @@ $(function () {
   //!Aquí el dolar también sirve como selector.
   //! document.querySelector("#navbarToggle").addEventLister("blur...
   //* navbarToggle es el id del botón.
+  //* Blur es cuando pierde el foco un elemento.
   $("#navbarToggle").blur(function (event) {
     var screenWidth = window.innerWidth;
     if (screenWidth < 768) {
@@ -11,5 +12,15 @@ $(function () {
       //* Este es el id de todo nuestro menú. La lista.
       $("#collapsable-nav").collapse("hide");
     }
+  });
+
+  // In Firefox and Safari, the click event doesn't retain the focus
+  // on the clicked button. Therefore, the blur event will not fire on
+  // user clicking somewhere else in the page and the blur event handler
+  // which is set up above will not be called.
+  // Refer to issue #28 in the repo.
+  // Solution: force focus on the element that the click event fired on
+  $("#navbarToggle").click(function (event) {
+    $(event.target).focus();
   });
 });
