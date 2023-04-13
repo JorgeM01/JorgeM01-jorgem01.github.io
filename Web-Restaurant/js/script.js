b8d4b36d21ef8d9612b65643ed40d8e327fae8ed
JorgeML01 <jorgedd2001@gmail.com>
2023-04-12 22:25:16 -0600

    ajax items connection

--- Web-Restaurant/js/script.js
+++ Web-Restaurant/js/script.js
@@ -31,7 +31,12 @@ $(function () {
 //! Función para cargar el main content.
 (function (global) {
   var dc = {};
+
   var homeHtml = "snippets/home-snippet.html";
+  var allCategoriesURL =
+    "https://coursera-jhu-default-rtdb.firebaseio.com/categories.json";
+  var categoriesTitleHTML = "snippets/categories-title-snippet.html";
+  var categoryHTML = "snippets/category-snippet.html";
 
   // Convinience function for inserting innerHTML for 'select'.
   var insertHtml = function (selector, html) {
@@ -46,6 +51,21 @@ $(function () {
     insertHtml(selector, html);
   };
 
+  /**
+   * !La letra "g" se utiliza en la expresión regular como una "bandera" o "flag" para indicar
+   * !que se debe buscar todas las ocurrencias de la cadena que coinciden con el patrón en lugar de
+   * !solo la primera ocurrencia.
+   * */
+  // Return substitute of '{{propname}} with propValue is given 'string'.
+  var insertProperty = function (string, propName, propValue) {
+    var propToReplace = "{{" + propName + "}}";
+    string = string.replace(
+      new RegExp(propToReplace, "g", propValue),
+      propValue
+    );
+    return string;
+  };
+
   // On page load (before images or CSS).
   document.addEventListener("DOMContentLoaded", function (event) {
     showLoading("#main-content");
@@ -58,6 +78,60 @@ $(function () {
       false
     );
   });
+
+  dc.loadMenuCategories = function () {
+    showLoading("#main-content");
+    //! No se le pasó ni true ni false porque por defecto es jason.
+    $ajaxUtils.sendGetRequest(allCategoriesURL, buildAndShowCategoriesHTML);
+  };
+
+  function buildAndShowCategoriesHTML(categories) {
+    // Load title snippet of categories page.
+    $ajaxUtils.sendGetRequest(
+      categoriesTitleHTML,
+      function (categoriesTitleHTML) {
+        // Retrieve single category snippet.
+        $ajaxUtils.sendGetRequest(
+          categoryHTML,
+          function (categoryHTML) {
+            var categoriesViewHTML = buildCategoriesViewHTML(
+              categories,
+              categoriesTitleHTML,
+              categoryHTML
+            );
+            insertHtml("#main-content", categoriesViewHTML);
+          },
+          false
+        );
+      },
+      false
+    );
+  }
+
+  function buildCategoriesViewHTML(
+    categories,
+    categoriesTitleHTML,
+    categoryHTML
+  ) {
+    var finalHTML = categoriesTitleHTML;
+    finalHTML += "<section class='row'>";
+
+    // Loop for categories.
+    for (var i = 0; i < categories.length; i++) {
+      // Insert category values.
+      var html = categoryHTML;
+      var name = "" + categories[i].name;
+      var short_name = categories[i].short_name;
+      // Es como un replace. Es la función que hicimos arriba.
+      html = insertProperty(html, "name", name);
+      html = insertProperty(html, "short_name", short_name);
+
+      finalHTML += html;
+    }
+
+    finalHTML += "</section>";
+    return finalHTML;
+  }
 
   global.$dc = dc;
 })(window);
